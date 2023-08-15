c2df523a04928e96e2ac7f6293c857adea150329
garamgim <garamgim@gmail.com>
2023-08-15 18:56:06 +0900

    Add grid size slider & clear button

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 
 
-// Making a 16 x 16 pallete
 
-const containerDiv = document.querySelector('.grid-container');
+// Initialize
 
-let gridNum = 16;
+let containerDiv = document.querySelector('.grid-container');
+let gridDivs = document.querySelectorAll('.grid-child');
 let pencilColor = '#a7a790';
 
-for (let i = 1; i <= gridNum * gridNum; i++) {
+makeGrid(16);
 
-    let div = document.createElement('div');
-    div.className = 'grid-child';
-    div.setAttribute('id', `${i}`);
-    containerDiv.appendChild(div);
 
-    div.addEventListener('mouseenter', (e) => {
-        if (drawing === true) {
-            e.target.style.backgroundColor = pencilColor;
-        }
-    });
+
+// Grid making function
+
+function makeGrid(num) {
+    for (let i = 1; i <= num * num; i++) {
+
+        let div = document.createElement('div');
+        div.className = 'grid-child';
+        div.setAttribute('id', `${i}`);
+        div.style.flex = `calc(70vh / ${num})`;
+        containerDiv.appendChild(div);
+
+        div.addEventListener('mouseenter', (e) => {
+            if (drawing === true) {
+                e.target.style.backgroundColor = pencilColor;
+            }
+        });
+    }
 }
+
 
 
 // Drawing toggle
@@ -29,4 +39,21 @@ let drawing = false;
 document.addEventListener('mousedown', () => { drawing = true; });
 document.addEventListener('mouseup', () => { drawing = false; });
 
-//토글 개념 사용, https://stackoverflow.com/questions/48593312/javascript-event-when-mouseover-and-mousedown
+
+
+// Slider value update
+
+function updateInput(val) {
+    containerDiv.innerHTML = '';
+    makeGrid(val);
+    document.getElementById('size').innerHTML = `${val} X ${val}`;
+}
+
+
+
+// Refresh button
+
+const refreshButton = document.getElementById('refresh');
+refreshButton.addEventListener('click', () => {
+    gridDivs.forEach(grid => grid.style.backgroundColor = '#f9f9e1');
+});
